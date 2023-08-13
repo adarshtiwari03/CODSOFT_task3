133462220292373b1a99dcf9345f9b142bab288b
adarshtiwari03 <140322905+adarshtiwari03@users.noreply.github.com>
2023-08-13 23:02:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//@codewithcurious.com
+
+let expression = '';
+addEventListener
+
+//confirm(" Do you want to open digital calculator");
+
+function appendNumber(number) {
+  expression += number;
+  document.getElementById('result').value = expression;
+}
+
+function appendOperator(operator) {
+  expression += operator;
+  document.getElementById('result').value = expression;
+}
+
+function clearResult() {
+  expression = '';
+  document.getElementById('result').value = '';
+}
+
+function calculate() {
+  try {
+    const result = eval(expression);
+    document.getElementById('result').value = result;
+    expression = '';
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+  }
+
+
+}
